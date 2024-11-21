64d4f4094357fb6f9fe79beb323b4d90e8527ab6
abychkova2 <abychkova2@rencredit.ru>
2024-11-21 12:33:15 -0500

    Button component adding forwardRef

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { forwardRef, useMemo } from 'react'
 
 import { classNames } from 'utils/classNames'
 
@@ -8,38 +8,45 @@ import { Spinner } from '../Spinner'
 
 import styles from './styles.module.css'
 
-export const Button = ({
-	size = 'medium',
-	radius = 'round',
-	variant = 'primary',
-	label,
-	loading = false,
-	startIcon,
-	endIcon,
-	fullWidth,
-	...props
-}: ButtonProps) => {
-	const className = classNames(
-		styles.button,
-		fullWidth && styles.fullWidth,
-		buttonClassByRadius[radius],
-		buttonClassByVariant[variant],
-		buttonClassBySize[size],
-	)
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+	(
+		{
+			size = 'medium',
+			radius = 'round',
+			variant = 'primary',
+			label,
+			loading = false,
+			startIcon,
+			endIcon,
+			fullWidth,
+			...props
+		},
+		ref,
+	) => {
+		const className = classNames(
+			styles.button,
+			fullWidth && styles.fullWidth,
+			buttonClassByRadius[radius],
+			buttonClassByVariant[variant],
+			buttonClassBySize[size],
+		)
 
-	const loader = useMemo(() => <Spinner color={'#74c69d'} size={buttonIconSizes[size]} />, [size])
+		const loader = useMemo(() => <Spinner size={buttonIconSizes[size]} />, [size])
 
-	return (
-		<button className={className} {...props}>
-			{loading ? (
-				loader
-			) : (
-				<>
-					{startIcon}
-					{label}
-					{endIcon}
-				</>
-			)}
-		</button>
-	)
-}
+		return (
+			<button className={className} ref={ref} {...props}>
+				{loading ? (
+					loader
+				) : (
+					<>
+						{startIcon}
+						{label}
+						{endIcon}
+					</>
+				)}
+			</button>
+		)
+	},
+)
+
+Button.displayName = 'Button'
