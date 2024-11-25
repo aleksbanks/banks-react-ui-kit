d8fba78086f214e36be7d0a41b233514cd00e0f7
abychkova2 <abychkova2@rencredit.ru>
2024-11-25 17:26:57 -0500

    Dots Loader component

--- src/components/DotsLoader/DotsLoader.tsx
+++ src/components/DotsLoader/DotsLoader.tsx
@@ -1,17 +1,18 @@
 import React, { forwardRef } from 'react'
+
 import { classNames } from 'utils'
 
+import styles from './DotsLoader.module.css'
 import type { DotsLoaderProps } from './DotsLoader.types'
 
-import styles from './DotsLoader.module.css'
-
-
 export const DotsLoader = forwardRef<HTMLDivElement, DotsLoaderProps>(({ size = 'm', ...props }, ref) => {
-  return (
-    <div ref={ref} {...props}>
-      <span className={classNames(styles.dot, styles[`dot-${size}`])}/>
-      <span className={classNames(styles.dot, styles[`dot-${size}`])}/>
-      <span className={classNames(styles.dot, styles[`dot-${size}`])}/>
-    </div>
-  )
+	return (
+		<div className={styles.loaderWrapper} ref={ref} {...props}>
+			<span className={classNames(styles.dot, styles[`dot-${size}`])} />
+			<span className={classNames(styles.dot, styles[`dot-${size}`])} />
+			<span className={classNames(styles.dot, styles[`dot-${size}`])} />
+		</div>
+	)
 })
+
+DotsLoader.displayName = 'DotsLoader'
--- src/components/DotsLoader/DotsLoader.types.ts
+++ src/components/DotsLoader/DotsLoader.types.ts
@@ -1,8 +1,12 @@
 import type { HTMLAttributes } from 'react'
+
 import type { Size } from 'types/common.types'
 
-export type DotsLoaderSize = Extract<Size,'s' | 'm'>
+export type DotsLoaderSize = Extract<Size, 's' | 'm'>
 
 export interface DotsLoaderProps extends HTMLAttributes<HTMLDivElement> {
-	size?: DotsLoaderSize;
+	/** Loader size
+	 * @default 'm'
+	 * */
+	size?: DotsLoaderSize
 }
